10cbe7ccd7f61cd57183a62d4a971ab58fdf1e59
Toinn <ngoctoi.it@gmail.com>
2018-07-17 09:25:53 -0700

    update time cronjob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('video:start')
-            ->hourlyAt(10);
+            ->hourly();
         $schedule->command('video:check')
             ->everyFiveMinutes();
     }
