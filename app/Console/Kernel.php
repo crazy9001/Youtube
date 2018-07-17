fe9334b6008a6d4eb26227b74a9cf9ff813614d7
Toinn <ngoctoi.it@gmail.com>
2018-07-17 08:32:55 -0700

    tét cronjob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('video:start')
-            ->hourlyAt(30);
+            ->hourlyAt(34);
         $schedule->command('video:check')
             ->everyFiveMinutes();
     }
